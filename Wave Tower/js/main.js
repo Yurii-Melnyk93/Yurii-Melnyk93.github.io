6706618c30a0cae9568ca7b2a7246c49a265f93f
Yurii-Melnyk93 <56911663+Yurii-Melnyk93@users.noreply.github.com>
2021-09-01 11:58:25 +0300

    Add files via upload

---
+++ Wave Tower/js/main.js
@@ -0,0 +1,64 @@
+$(function() {
+    ////////////////
+	 $('.slider').slick({
+  slidesToShow: 1,
+  infinite: true,
+  speed: 300,
+  slidesToScroll: 1,
+  arrows: false,
+  variableWidth: true,
+//   responsive: [{
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//       }
+//     }
+//   ]
+});
+	////////////////////
+
+
+	
+jQuery(window).scroll(function(){
+         var $sections = $('section');
+	$sections.each(function(i,el){
+        var top  = $(el).offset().top-100;
+        var bottom = top +$(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+    	if( scroll > top && scroll < bottom){
+            $('a.active').removeClass('active');
+			$('a[href="#'+id+'"]').addClass('active');
+
+        }
+    })
+ });
+
+$("#scrollNav").on("click","a", function (event) {
+        // исключаем стандартную реакцию браузера
+        event.preventDefault();
+ 
+        // получем идентификатор блока из атрибута href
+        var id  = $(this).attr('href'),
+ 
+        // находим высоту, на которой расположен блок
+            top = $(id).offset().top;
+         
+        // анимируем переход к блоку, время: 800 мс
+        $('body,html').animate({scrollTop: top}, 200);
+    });
+	
+});
+
