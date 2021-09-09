e596eaea27093c51e7b543c6fa0f9a1ac1ad6f9c
Yurii-Melnyk93 <56911663+Yurii-Melnyk93@users.noreply.github.com>
2021-09-09 19:35:11 +0300

    Add files via upload

--- Project303/js/main.js
+++ Project303/js/main.js
@@ -1,6 +1,40 @@
 $(function() {
 	
+    //////////////////
+  $("#form").submit(function (e) { // Устанавливаем событие отправки для формы с id=form
+           e.preventDefault();
+            var formData = $(this).serialize(); // Собираем все данные из формы
+            $.ajax({
+                type: "POST", // Метод отправки
+                url: "", // Путь до php файла отправителя
+                data: formData,
+                success: function () {
+                    // Код в этом блоке выполняется при успешной отправке сообщения
+                    // alert("Ваше сообщение отправлено!");
+                    $('#form').trigger( 'reset' );
+                }
+            });
+            $('#form').trigger( 'reset' );
+            return false;
+        });   
+
+//////////////////////
+
 	
 	
 	
 });
+
+
+////////////
+function checkParams() {
+    var name = $('#name').val();
+    var email = $('#mail').val();
+    var phone = $('#phone').val();
+
+    if(name.length != 0 && email.length != 0 && phone.length != 0) {
+        $('#submit-btn').removeAttr('disabled');
+    } else {
+        $('#submit-btn').attr('disabled', 'disabled');
+    }
+}
